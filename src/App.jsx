0035c49a946fc2516b247abfbf5b77efb3199d4f
Your Name <you@example.com>
2024-03-30 19:39:45 +0700

    added aos

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,13 @@ import renderRoutes from "./routes";
 import { Suspense } from "react";
 import Loading from "./components/Loading";
 
+import AOS from "aos";
+import "aos/dist/aos.css";
 function App() {
+  useEffect(() => {
+    AOS.init({});
+  }, []);
+
   return (
     <Suspense fallback={<Loading />}>
       <BrowserRouter>
